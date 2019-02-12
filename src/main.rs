1221da86bffe5f53a5ab5794cfc165b00b15f622
lemoce <cerencio@yahoo.com.br>
2019-02-12 17:27:00 -0200

    ajuste no download e copia do arquivo

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,8 @@ extern crate sdl2;
 extern crate select;
 extern crate tempfile;
 
-use std::path::Path;
+use std::fs::File;
+use std::path::{Path, PathBuf};
 use std::string::String;
 use std::result::Result;
 
@@ -11,12 +12,14 @@ use select::document::Document;
 use select::node::Node;
 use select::predicate::{Class, Name};
 
-use tempfile::NamedTempFile;
+use tempfile::Builder;
 
 use sdl2::image::{LoadTexture, InitFlag};
 use sdl2::render::WindowCanvas;
 use sdl2::event::{Event, WindowEvent};
 use sdl2::keyboard::Keycode;
+
+use uuid::Uuid;
 
 fn draw_image(path: &Path, canvas: &mut WindowCanvas) -> Result<(), String> {
     let texture_creator = canvas.texture_creator();
@@ -28,7 +31,7 @@ fn draw_image(path: &Path, canvas: &mut WindowCanvas) -> Result<(), String> {
     Ok(())
 }
 
-fn run(image_files: &Vec<&NamedTempFile>) -> Result<(), String> {
+fn run(image_files: &Vec<&Path>) -> Result<(), String> {
     let sdl_context = sdl2::init()?;
     let video_subsystem = sdl_context.video()?;
     let _image_context = sdl2::image::init(InitFlag::PNG | InitFlag::JPG)?;
@@ -41,7 +44,7 @@ fn run(image_files: &Vec<&NamedTempFile>) -> Result<(), String> {
 
     let mut idx = 0;
     
-    draw_image(image_files[idx].path(), &mut canvas)?;
+    draw_image(image_files[idx], &mut canvas)?;
     
     'mainloop: loop {
         for event in sdl_context.event_pump()?.poll_iter() {
@@ -52,17 +55,17 @@ fn run(image_files: &Vec<&NamedTempFile>) -> Result<(), String> {
                 Event::KeyDown {keycode: Option::Some(Keycode::Right), ..} => {
                     if idx < 4 {
                         idx = idx + 1;
-                        draw_image(image_files[idx].path(), &mut canvas)?;
+                        draw_image(image_files[idx], &mut canvas)?;
                     }
                 },
                 Event::KeyDown {keycode: Option::Some(Keycode::Left), ..} => {
                     if idx > 0 {
                         idx = idx - 1;
-                        draw_image(image_files[idx].path(), &mut canvas)?;
+                        draw_image(image_files[idx], &mut canvas)?;
                     }
                 },
                 Event::Window{ win_event: WindowEvent::Exposed, .. } => 
-                    draw_image(image_files[idx].path(), &mut canvas)?,
+                    draw_image(image_files[idx], &mut canvas)?,
                 _ => { }
             }
         }
@@ -100,19 +103,27 @@ fn main() {
 
     let image_urls = extract_image_urls(get_response_body().unwrap());
 
-    let mut file_vec: Vec<NamedTempFile> = vec![];
+    let mut file_vec: Vec<Box<PathBuf>> = vec![];
 
+    let dir = Builder::new()
+        .prefix("tirinha")
+        .suffix("-files")
+        .tempdir()
+        .unwrap();
+    
     for _ in 0 .. image_urls.len() {
-        let file = NamedTempFile::new().unwrap();
-        file_vec.push(file);
+        let file = dir.path().join(format!("{}.jpg", Uuid::new_v4().to_hyphenated()));
+        file_vec.push(Box::new(file));
     }
 
     let zip_urls_files = image_urls.iter().zip(file_vec.iter());
 
-    let downloaded_files: Vec<&NamedTempFile> = zip_urls_files.map(|(url, mut f)| {
+    let downloaded_files: Vec<&Path> = zip_urls_files.map(|(url, path)| {
+        let mut file = File::create(path.as_path()).unwrap();
         let mut resp = reqwest::get(url.as_str()).unwrap();
-        resp.copy_to(&mut f).unwrap();
-        f
+        resp.copy_to(&mut file).unwrap();
+        drop(file);
+        path.as_path()
     }).collect();
 
     run(&downloaded_files).unwrap();
